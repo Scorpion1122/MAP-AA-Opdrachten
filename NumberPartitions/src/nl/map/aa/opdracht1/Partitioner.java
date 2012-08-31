f42a0887e324dafc02b4f19fdd51a2cea6642bba
Niek <niekversteege@gmail.com>
2012-08-31 11:47:30 +0200

    - Voorbeeld van website opgenomen in class.

--- NumberPartitions/src/nl/map/aa/opdracht1/Partitioner.java
+++ NumberPartitions/src/nl/map/aa/opdracht1/Partitioner.java
@@ -1,44 +1,29 @@
 package nl.map.aa.opdracht1;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Partitioner
 {
-	private List<String> partitionStrings;
-
 	public Partitioner()
 	{
-		partitionStrings = new ArrayList<String>();
+		
 	}
-	
+
 	public void start(final int numberToPartition)
 	{
-		partitionStrings.addAll(getPartitionStrings(numberToPartition));
-		printPartitions(numberToPartition);
+		partition(numberToPartition, numberToPartition, "");
 	}
 
-	private void printPartitions(int numberToPartition)
+	private void partition(int numberToPartition, int max, String prefix)
 	{
-		System.out.println("Partitions for the number " + numberToPartition + ":");
-		
-		for (String s : partitionStrings)
+		if (numberToPartition == 0)
 		{
-			System.out.println(s);
+			System.out.println(prefix);
+			return;
+		}
+
+		for (int i = Math.min(max, numberToPartition); i >= 1; i--)
+		{
+			partition(numberToPartition - i, i, prefix + " " + i);
 		}
 	}
-
-	/**
-	 * for every possible partition: return a printable string with the numbers
-	 * in the partition.
-	 * 
-	 * @param numberToPartition
-	 * @return
-	 */
-	private List<String> getPartitionStrings(final int numberToPartition)
-	{
-		//TODO: alles!
-		return null;
-	}
-
 }
